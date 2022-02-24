7e8054c63fba0d15733e655537c088df2d343c41
Andrew <81114540+Drucifer2082@users.noreply.github.com>
2022-02-24 11:37:38 -0800

    Update settings.py
    So there are 18 commits etc here now   wasn't sure which one to do the PR from so I chose settings as I haven't yet copied over the secret key and changed debug to false  (following the directions of the video)

--- web_project/settings.py
+++ web_project/settings.py
@@ -16,9 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_ki+8i)ql#3nxew)ll6!67_##^l9%u$q0*tu(c$x_&)272f%py'
 
