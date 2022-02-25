0b331748722c60f1425e4dca332a277fb617c766
Bob Belderbos <bobbelderbos@gmail.com>
2022-02-25 20:06:31 +0100

    added todos

--- news/views.py
+++ news/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 from .models import Article
 
 
-def index(response, id):
-    article = Article.objects.get(id=id)
-    return render(response, "main/list.html", {"article": article})
+def index(response):
+    articles = Article.objects.all()
+    # TODO:
+    """
+    1. we need to put the api scraping code in django
+    2. call it and populate db articles table
+    3. v) we already retrieve them in the view
+    4. display articles in view
+    5. look at django forms to have a user do a search
+    """
+    return render(response, "main/list.html", {"articles": articles})
 
 
 def home(response):
