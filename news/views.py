959a6739bc44ab942b05bc1c1b0e105571e8afd1
Andrew F <ravens2082@outlook.com>
2022-02-24 16:53:48 -0800

    django views
    worked on setting up some django views.   i probably went page happy lol.  but its an easy fix

--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
+from subprocess import IDLE_PRIORITY_CLASS
 from django.shortcuts import render
 
 # Create your views here.
+def index(response, id):
+    ls = NewsList.objects.get(id=id)
+    return render(response, "main/list.html", {"ls":ls})
+
+
+def home(response):
+    return render(response, "main/home.html", {})
--- web_project/settings.py
+++ web_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'news',
+]   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+]   
 
 ROOT_URLCONF = 'web_project.urls'
 
