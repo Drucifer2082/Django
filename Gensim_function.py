3ccaa1ed689183d2a8a4dd88de6a33e918b99cdd
Andrew <81114540+Drucifer2082@users.noreply.github.com>
2022-03-01 12:50:33 -0800

    Gensim_tokenization
    So, this works for a string and a dictionary of strings (in Jupyter).  However,  I am looking into the API call.   I was also unsure if this should be one function or well,  how to break down the analysis into functions etc.   I think docstrings are going to be important in this one.

---
+++ Gensim_function.py
@@ -0,0 +1,27 @@
+from collections import defaltdict
+from gensim import corpora
+
+article = ""
+article_corpus = []
+
+# Creates a set of frequent words
+stoplist = set('for a of the and to in'.split(' '))
+# Lowercase each document, split it by white space and filter out stopwords
+texts = [[word for word in document.lower().split() if word not in stoplist]
+         for document in article_corpus]
+
+# Counts word frequencies
+frequency = defaultdict(int)
+for text in texts:
+    for token in text:
+        frequency[token] += 1
+
+# Only keep words that appear more than once
+processed_corpus = [[token for token in text if frequency[token] > 1] for text in texts]
+pprint.pprint(processed_corpus)
+
+"""corpora associates each word in the corpus with a unique integer ID. 
+This dictionary defines the vocabulary of all words that our processing knows about."""
+
+article_identifiers = corpora.Dictionary(processed_corpus)
+print(article_identifiers.token2id)
