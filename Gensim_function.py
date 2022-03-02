a96d8fd6f31dac3ea427e07159c4a92ad9a4a081
Andrew <81114540+Drucifer2082@users.noreply.github.com>
2022-03-02 10:36:13 -0800

    Gensim_analysis_TFIDF
    This entire process takes each document and performs an analysis of the original document/documents.   It returns a percentage of similarity between documents.

--- Gensim_function.py
+++ Gensim_function.py
@@ -1,5 +1,5 @@
-from collections import defaltdict
-from gensim import corpora
+from collections import defaultdict
+from gensim import corpora, models, similarities
 
 article = ""
 article_corpus = []
@@ -11,7 +11,7 @@ texts = [[word for word in article.lower().split() if word not in stoplist]
          for article in article_corpus]
 
 # Counts word frequencies
-frequency = defaultdict(int)
+frequency = colctions.Counter(int)
 for text in texts:
     for token in text:
         frequency[token] += 1
@@ -25,3 +25,38 @@ This dictionary defines the vocabulary of all words that our processing knows ab
 
 article_identifiers = corpora.Dictionary(processed_corpus)
 print(article_identifiers.token2id)
+
+dictionary = corpora.Dictionary(processed_corpus)
+print(dictionary.token2id)
+
+
+new_search = ""
+new_vec = dictionary.doc2bow(new_search.lower().split())
+print(new_vec)
+
+bow_corpus = [dictionary.doc2bow(text) for text in processed_corpus]
+pprint.pprint(bow_corpus)
+
+
+# train the model
+tfidf = models.TfidfModel(bow_corpus)
+
+# transform the "system minors" string
+words = "system minors".lower().split()
+print(tfidf[dictionary.doc2bow(words)])
+
+
+"""Transforms the entire corpus with TFIDF"""
+index = similarities.SparseMatrixSimilarity(tfidf[bow_corpus], num_features=12)
+
+
+"""Queries the simiarities between documents in the corpus;  returns a percent"""
+
+query = ''.split()
+query_bow = dictionary.doc2bow(query)
+sims = index[tfidf[query_bow]]
+print(list(enumerate(sims)))
+
+for article_number, score in sorted(enumerate(sims), key=lambda x: x[1], reverse=True):
+    print(article_number, score)
+
