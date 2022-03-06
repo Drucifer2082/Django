b98392da33eb32561d531564c36200c4d832c320
Andrew F <ravens2082@outlook.com>
2022-03-06 15:34:44 -0800

    completed the parsing and ML back end.   Just need to connect the two

--- scraper.py
+++ scraper.py
@@ -3,8 +3,7 @@ import re
 import requests
 
 guardian_api_key = "725f716b-09bf-4971-816f-ef6b32061b1b"
-historical_news_rapidapi_key = "38c3a26bf6msh7b78364c9cb74afp18a2ddjsnef679721e0c0"
-google_news_rapidapi_key = "38c3a26bf6msh7b78364c9cb74afp18a2ddjsnef679721e0c0"
+nytimes_historical_api = "K6uJovBQ20GvZDB6e9wAWeoYO6m21rNY"
 
 
 class NoContentException(Exception):
@@ -18,9 +17,19 @@ def get_theguardian_articles(search):
     guardian_results = query.json()
     return guardian_results
 
+def get_theguardian_titles(search):
+    url = f"https://content.guardianapis.com/search?q={search}&api-key={guardian_api_key}&show-blocks=all"
+    query = requests.get(url)
+    ret = query.json()
+    try:
+        article_title = ret['response']['results'][0]['webTitle']
+    except IndexError:
+        raise NoContentException(f"cannot retrieve article content of {url}")    
+    
+    return article_title
 
-def parse_theguardian_article(url):
-    url = f"{url}?api-key={guardian_api_key}&show-blocks=all"
+def parse_theguardian_article(search):
+    url = f"https://content.guardianapis.com/search?q={search}&api-key={guardian_api_key}&show-blocks=all"
     query = requests.get(url)
     ret = query.json()
     try:
@@ -32,33 +41,57 @@ def parse_theguardian_article(url):
 
 
 def historical_news_api(search):
-    """API key and query search from Rapida API on historical news"""
-    url = "https://historical-news.p.rapidapi.com/reuters/2021-01-01/"
-    headers = {
-        'x-rapidapi-host': "historical-news.p.rapidapi.com",
-        'x_rapidapi_key': historical_news_rapidapi_key
-    }
-    historical_news_results = requests.request("GET", url, headers=headers, params=search)
+    url = f"https://api.nytimes.com/svc/search/v2/articlesearch.json?q={search}&api-key={nytimes_historical_api}&show-blocks=all"
+    query = requests.get(url)
+    nytimes_historical_results = query.json()
+    return nytimes_historical_results
+    
+def parse_historical_news_api_headline(nytimes_results, nytimes_historical_results):
+    nytimes_results_print_headline = nytimes_results['response']['docs'][0]['headline']['print_headline']
+    return nytimes_results_print_headline
 
-    return historical_news_results
+def parse_historical_news_api_section(nytimes_results, nytimes_historical_results):
+    nytimes_results_section_name = nytimes_results['response']['docs'][0]['section_name']
+    return nytimes_results_section_name
 
-def parse_historical_news_api(url)
+def parse_historical_news_api_lead_paragraph(nytimes_results, nytimes_historical_results):
+    nytimes_results_lead_paragraph = nytimes_results['response']['docs'][0]['section_name']
+    return nytimes_results_lead_paragraph
 
+def daily_top_headlines():
+    """ shows live headlines in near real time."""
+    url = ('https://newsapi.org/v2/top-headlines?'
+           'country=us&'
+           'apiKey=67a718ca0c904911b885f859c255dc21')
+    response = requests.get(url)
+    top_headlines = response.json()
+    return top_headlines
 
+def parse_daily_top_headlines_titles():
+    url = url = ('https://newsapi.org/v2/top-headlines?'
+           'country=us&'
+           'apiKey=67a718ca0c904911b885f859c255dc21')
+    query = requests.get(url)
+    ret = query.json()
+    try:
+        ret = ret["articles"][0]['title']
+    except IndexError:
+        raise NoContentException(f"cannot retrieve article content of {url}")
+    # strip html tags off
+    return re.sub('<[^<]+?>', '', ret)
 
-
-
-def google_news_api(search):
-    """Top stories, topic-related news feeds, a geolocation news feed, and an extensive full-text search feed"""
-    url = "https://google-news.p.rapidapi.com/v1/top_headlines"
-    headers = {
-        'x-rapidapi-host': "google-news.p.rapidapi.com",
-        'x-rapidapi-key': google_news_rapidapi_key
-    }
-    google_news_results = requests.request("GET", url, headers=headers, params=search)
-
-    return google_news_results
-
+def parse_daily_top_headlines_content():
+    url = url = ('https://newsapi.org/v2/top-headlines?'
+           'country=us&'
+           'apiKey=67a718ca0c904911b885f859c255dc21')
+    query = requests.get(url)
+    ret = query.json()
+    try:
+        ret = ret['articles'][0]['content']
+    except IndexError:
+        raise NoContentException(f"cannot retrieve article content of {url}")
+    # strip html tags off
+    return re.sub('<[^<]+?>', '', ret)
 
 if __name__ == "__main__":
     # you can run this script as: $ python -m news.scraper
