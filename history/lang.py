e924856784e5d84ed70f9f77294c4c8d282ce645
Andrew F <ravens2082@outlook.com>
2022-03-09 14:47:56 -0800

    flake 8 updating

--- history/lang.py
+++ history/lang.py
@@ -5,27 +5,33 @@ from gensim.parsing.preprocessing import remove_stopwords, preprocess_documents
 
 from .scraper import get_theguardian_articles, parse_theguardian_article
 
+
 def collecting_articles(article):
-    """building the corpus for  ML"""
+    """building the corpus model for  ML"""
     article_corpus = [article]
     return article_corpus
+
 
 def telegraphic_text_single_article(article):
     """removes stopwords, predefined by Gensim, across the text."""
     filtered_article = remove_stopwords(article)
     return filtered_article
 
+
 def telegraphic_text_multiple_articles(article_corpus):
     """removes stopwords across multiple texts."""
     post_processed_articles = preprocess_documents(article_corpus)
     return post_processed_articles
 
+
 def article_analysis(filtered_article, article_corpus):
-    """The Natural Language Processing analysis on the articles....
-    returns a dictionary bag-of-words vector"""
-    # Creates an additional set of stop words, final check to make sure some words do not get through
+    """The Natural Language Processing analysis on the 
+    articles, returns a dictionary bag-of-words vector"""
+    """Creates an additional set of stop words, final
+    check to make sure some words do not get through"""
     stoplist = set('for a of the and to in but'.split(' '))
-    # Lowercase each document, split it by white space and filter out stopwords
+    # Lowercase each document, split it by white
+    # space and filter out stopwords
     texts = [[word for word in filtered_article.lower().split() if word not in stoplist]
              for article in article_corpus]
 
@@ -38,7 +44,8 @@ def article_analysis(filtered_article, article_corpus):
     # Only keeps words that appear more than once
     processed_article = [[token for token in text if frequency[token] > 1] for text in texts]
     return processed_article
-    
+
+
 def selected_article_topical_keywords(processed_article):
     stoplist = set('for a of the and to in but'.split(' '))
     dictionary = corpora.Dictionary(processed_article)
@@ -49,21 +56,26 @@ def selected_article_topical_keywords(processed_article):
         if stopword in dictionary.token2id
         ]
     once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.items() if docfreq == 1]
-    dictionary.filter_tokens(stop_ids + once_ids)  
-    dictionary.compactify()  
+    dictionary.filter_tokens(stop_ids + once_ids)
+    dictionary.compactify()
     article_keyword_dictionary = dictionary
     return article_keyword_dictionary
+
 
 def keywords_into_vectors(processed_article):
     dictionary = corpora.Dictionary(processed_article)
     bow_corpus = [dictionary.doc2bow(text) for text in processed_article]
     return bow_corpus
 
+
 def training_the_model(bow_corpus):
-    """TF-IDF is a statistical measure that evaluates how relevant a word is to a document in a collection of documents."""
+    """TF-IDF is a statistical measure that evaluates 
+    how relevant a word is to a document in a 
+    collection of documents."""
     tfidf = models.TfidfModel(bow_corpus)
     index = similarities.SparseMatrixSimilarity(tfidf[bow_corpus], num_features=12)
     return index, tfidf
+
 
 def searching_the_articles(index, tfidf, processed_article):
     dictionary = corpora.Dictionary(processed_article)
