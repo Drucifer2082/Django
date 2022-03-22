2f7ff406cb2aec84daba7f5c05b5901435759c8c
Bob Belderbos <bobbelderbos@gmail.com>
2022-03-22 13:33:40 +0100

    support 3.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="article-history",
-    version="1.0.0",
+    version="1.0.1",
     author="Andrew and Bob @ PDM",
     author_email="support@pybit.es",
     description="A tool to find similarity between (historic) articles",
@@ -23,7 +23,7 @@ setuptools.setup(
     packages=["history"],
     include_package_data=True,
     install_requires=["requests", "bs4", "gensim", "python-dotenv"],
-    python_requires=">=3.10",
+    python_requires=">=3.9",
     entry_points={
         "console_scripts": [
             "history=history.__main__:main",
