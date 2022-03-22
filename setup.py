4c262899ee4760d27d452e378a89b34c401b558c
Bob Belderbos <bobbelderbos@gmail.com>
2022-03-22 13:07:12 +0100

    add setup

--- setup.py
+++ setup.py
@@ -4,23 +4,29 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="",
-    version="0.0.1",
-    author="Bob B and Andrew F",
+    name="article-history",
+    version="1.0.0",
+    author="Andrew and Bob @ PDM",
     author_email="support@pybit.es",
-    description="A package to identify similar articles using Gensim",
+    description="A tool to find similarity between (historic) articles",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Drucifer2082/Django",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/Drucifer2082/Django",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    packages=["history"],
+    include_package_data=True,
+    install_requires=["requests", "bs4", "gensim", "python-dotenv"],
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+            "history=history.__main__:main",
+        ]
+    },
 )
